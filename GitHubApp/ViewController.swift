3c80eb440346e8671d40a446850a559bb64fbfc2
RyGuy2015 <ry.ingram01@gmail.com>
2016-08-17 20:54:00 -0400

    Initial Commit

---
+++ GitHubApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubApp
+//
+//  Created by Ryan Ingram on 8/17/16.
+//  Copyright © 2016 DPS Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
